be5043d2c6976b58465cd65a80ed9c2d97d8f7f6
Aldo Rizki Adi Wirastomo <rizkiealdo@gmail.com>
2025-08-05 22:41:34 +0700

    add rules

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -23,8 +23,24 @@ class ProfileUpdateRequest extends FormRequest
                 'lowercase',
                 'email',
                 'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
+                Rule::unique('users', 'username')->ignore($this->user()->id),
             ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nama lengkap wajib diisi.',
+            'name.string' => 'Nama lengkap harus berupa teks.',
+            'name.max' => 'Nama lengkap tidak boleh lebih dari 255 karakter.',
+
+            'email.required' => 'Email wajib diisi.',
+            'email.string' => 'Email harus berupa teks.',
+            'email.lowercase' => 'Email harus menggunakan huruf kecil.',
+            'email.email' => 'Format email tidak valid.',
+            'email.max' => 'Email tidak boleh lebih dari 255 karakter.',
+            'email.unique' => 'Email ini sudah digunakan.'
         ];
     }
 }
